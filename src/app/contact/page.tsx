76814483b24574a7a7456188049788fff3535820
pdrmenezes <pdrmenezes1@gmail.com>
2023-05-12 15:45:32 -0300

    styling contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,22 @@
 export default function ContactPage() {
   return (
-    <div>
-      <h1>contact</h1>
-      <div>
-        <div>
+    <div className="flex flex-col gap-2 h-full justify-center">
+      <h1 className="text-[clamp(3rem,9vw,6rem)] font-bold">contact</h1>
+      <div className="grid sm:grid-cols-1 sm:grid-rows-4  md:grid-cols-3 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-2 lg:gap-y-10 gap-5">
+        <div className="">
           <h2>EMAIL</h2>
           <p>pdrmenezes1@gmail.com</p>
+        </div>
+        <div className="col-span-2">
+          <h2>GITHUB</h2>
+          <p>/pdrmenezes</p>
+        </div>
+        <div className="">
           <h2>INSTAGRAM</h2>
           <p>@pdrmenezes</p>
         </div>
-        <div>
-          <h2>GITHUB</h2>
-          <p>/pdrmenezes</p>
-          <h2></h2>
+        <div className="col-span-2">
+          <h2>_</h2>
           <p>your turn (:</p>
         </div>
       </div>
