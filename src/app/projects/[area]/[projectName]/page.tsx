9c74a2ebd62ada43ab06e3d3a14979bd177b1cc2
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-04 21:52:26 -0300

    feat:project page + project list responsiveness

--- src/app/projects/[area]/[projectName]/page.tsx
+++ src/app/projects/[area]/[projectName]/page.tsx
@@ -1,4 +1,8 @@
 import type { Metadata, ResolvingMetadata } from "next";
+import { projectsData } from "../../data/projectsData";
+import Image from "next/image";
+import { DownArrow } from "@/assets/icons/DownArrow";
+import Link from "next/link";
 
 type Props = {
   params: { projectName: string };
@@ -20,10 +24,41 @@ export async function generateMetadata({ params }: Props, parent: ResolvingMetad
 export default function ProjectPage({ params }: Props) {
   const { projectName } = params;
 
+  const projectInfo = projectsData.find((project) => project.slug == projectName);
   const formattedProjectName = projectName
     .split("-")
     .map((word) => word[0].toLocaleUpperCase() + word.substring(1))
     .join(" ");
 
-  return <div>{formattedProjectName}</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <h3 className="text-3xl">
+          {projectInfo?.title} / {projectInfo?.year}{" "}
+          {projectInfo?.externalUrl && (
+            <Link href={projectInfo?.externalUrl} className="ml-2 inline-flex">
+              <DownArrow className="h-8 w-8 translate-y-1 -rotate-[135deg] stroke-whiteish stroke-2 group-hover:underline group-hover:underline-offset-4" />
+            </Link>
+          )}
+        </h3>
+
+        <h4 className="text-sm">{projectInfo?.content?.subtitle}</h4>
+      </div>
+
+      <div className="flex gap-4 border-t border-t-whiteish pt-5">
+        {projectInfo?.content?.tags?.map((tag) => (
+          <p key={tag} className="rounded-full bg-whiteish px-3 py-0.5 text-xs font-medium text-blackish">
+            {tag}
+          </p>
+        ))}
+      </div>
+
+      <p className="w-full sm:w-1/2">{projectInfo?.content?.description}</p>
+      <div className="grid gap-4 md:grid-cols-3">
+        {projectInfo?.content?.contentImagesSrc?.map((imageSrc, index) => (
+          <Image src={imageSrc} key={imageSrc} alt={`${projectInfo.title}&apos;s image ${index + 1}`} width={500} height={281} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/projects/data/projectsData.ts
+++ src/app/projects/data/projectsData.ts
@@ -6,8 +6,10 @@ export type TProjectData = {
   popoverImageSrc: string;
   popoverBgColor: string;
   altSlug?: string;
+  externalUrlOnly: boolean;
   externalUrl?: string;
   published: boolean;
+  featured: boolean;
   readonly slug: string;
   content?: {
     subtitle?: string;
@@ -25,20 +27,31 @@ export const projectsData: TProjectData[] = [
     roles: ["Creation", "Development"],
     popoverImageSrc: "memory-lake.webp",
     popoverBgColor: "#0000FF",
+    externalUrlOnly: false,
     externalUrl: "https://www.memorylake.cc",
-    published: false,
+    published: true,
+    featured: true,
     get slug() {
       return this.title.split(" ").join("-");
     },
+    content: {
+      subtitle: "a platform to save and share memories",
+      description:
+        "In computer science, a data lake is a centralized repository designed to store, process, and secure large amounts of structured, semistructured, and unstructured data. It can store data in its native format and process any variety of it. It means data lakes and our memories share things in common — both data and mementos can emerge depending on the input or moment we live in the present.",
+      contentImagesSrc: ["/projects/images/memory-lake.webp", "/projects/images/memory-lake.webp", "/projects/images/memory-lake.webp"],
+      tags: ["nextjs", "react", "figma"],
+    },
   },
   {
-    title: "Work in the last decade",
+    title: "Work In The Last Decade",
     year: "2023",
     areas: ["front-end", "design"],
     roles: ["Creation", "Design", "Development"],
     popoverImageSrc: "work.webp",
     popoverBgColor: "khaki",
+    externalUrlOnly: false,
     published: false,
+    featured: true,
     get slug() {
       return this.title.split(" ").join("-");
     },
@@ -50,10 +63,18 @@ export const projectsData: TProjectData[] = [
     roles: ["Development"],
     popoverImageSrc: "dh-money-house.webp",
     popoverBgColor: "#C1FD35",
+    externalUrlOnly: false,
     externalUrl: "https://github.com/ThiagoMaurat/dh-money",
     published: true,
+    featured: true,
     get slug() {
       return this.title.split(" ").join("-");
+    },
+    content: {
+      subtitle: "digital house's final project",
+      description: "A Digital Wallet so you never have to worry about carrying money again.",
+      contentImagesSrc: ["/projects/images/dh-money-house.webp", "/projects/images/dh-money-house.webp", "/projects/images/dh-money-house.webp"],
+      tags: ["nextjs", "react", "figma", "chakra ui"],
     },
   },
   {
@@ -63,10 +84,19 @@ export const projectsData: TProjectData[] = [
     roles: ["Design"],
     popoverImageSrc: "movimento-redes.webp",
     popoverBgColor: "#422969",
+    externalUrlOnly: false,
     externalUrl: "https://www.saudeecosol.org",
-    published: false,
+    published: true,
+    featured: true,
     get slug() {
       return this.title.split(" ").join("-");
+    },
+    content: {
+      subtitle: "NGO's website revamp",
+      description:
+        "Movimento Redes is an NGO that helps minorities get in the job market while also providing mental health support and developing cultural and educational activities",
+      contentImagesSrc: ["/projects/images/movimento-redes.webp", "/projects/images/movimento-redes.webp", "/projects/images/movimento-redes.webp"],
+      tags: ["figma", "material ui"],
     },
   },
   {
@@ -76,21 +106,25 @@ export const projectsData: TProjectData[] = [
     roles: ["Design", "Copy", "Development"],
     popoverImageSrc: "break-out.webp",
     popoverBgColor: "#F0F0F0",
+    externalUrlOnly: true,
     externalUrl: "https://pdrmenezes.github.io/scroll_animation-with-gsap/",
     published: true,
+    featured: false,
     get slug() {
       return this.title.split(" ").join("-");
     },
   },
   {
-    title: "Airport Flip Board animation",
+    title: "Airport Flip Board",
     year: "2022",
     areas: ["front-end, design"],
     roles: ["Design, Development"],
     popoverImageSrc: "airport-flip-board.webp",
     popoverBgColor: "#70cc84",
+    externalUrlOnly: true,
     externalUrl: "https://pdrmenezes.github.io/blured-mousetrail-effect-with-animated-text/",
     published: true,
+    featured: false,
     get slug() {
       return this.title.split(" ").join("-");
     },
@@ -102,8 +136,10 @@ export const projectsData: TProjectData[] = [
     roles: ["Development"],
     popoverImageSrc: "interactive-mouse-cursror.webp",
     popoverBgColor: "#F0F0F0",
+    externalUrlOnly: true,
     externalUrl: "https://pdrmenezes.github.io/Mouse-Trailing-Interactive-Mouse-Icons-w-CSS-JS/",
     published: true,
+    featured: false,
     get slug() {
       return this.title.split(" ").join("-");
     },
@@ -115,26 +151,15 @@ export const projectsData: TProjectData[] = [
     roles: ["Development"],
     popoverImageSrc: "wordle-like.webp",
     popoverBgColor: "tomato",
+    externalUrlOnly: true,
     externalUrl: "https://pdrmenezes.github.io/Wordle-Like-HTML-CSS-JS-/",
     published: true,
+    featured: false,
     get slug() {
       return this.title.split(" ").join("-");
     },
   },
-  {
-    title: "Iliminated Card Corners",
-    // thinking about a social project where we iluminate people's dangerous walks home
-    year: "2022",
-    areas: ["front-end"],
-    roles: ["Development"],
-    popoverImageSrc: "iluminated-corners.webp",
-    popoverBgColor: "#F5F5F5",
-    externalUrl: "https://pdrmenezes.github.io/CSS_JS_lit-up-cards-with-css-and-js/",
-    published: true,
-    get slug() {
-      return this.title.split(" ").join("-");
-    },
-  },
+
   {
     title: "Google's Dino Demo",
     // Best Chrome feature
@@ -143,12 +168,29 @@ export const projectsData: TProjectData[] = [
     roles: ["Development"],
     popoverImageSrc: "google-dino.webp",
     popoverBgColor: "#545454",
+    externalUrlOnly: true,
     externalUrl: "https://pdrmenezes.github.io/JS_Dino_Game/",
     published: true,
+    featured: false,
     get slug() {
       return this.title.split(" ").join("-");
     },
   },
+  // {
+  //   title: "Iluminated Card Corners",
+  //   // thinking about a social project where we iluminate people's dangerous walks home
+  //   year: "2022",
+  //   areas: ["front-end"],
+  //   roles: ["Development"],
+  //   popoverImageSrc: "iluminated-corners.webp",
+  //   popoverBgColor: "#F5F5F5",
+  //   externalUrl: "https://pdrmenezes.github.io/CSS_JS_lit-up-cards-with-css-and-js/",
+  //   published: true,
+  //   featured: false,
+  //   get slug() {
+  //     return this.title.split(" ").join("-");
+  //   },
+  // },
 ];
 
 // This very website
